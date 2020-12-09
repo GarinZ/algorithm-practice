d8296247594def3486a06cf4272dfc64467f88a5
Garin <454067820@qq.com>
2020-12-09 13:54:57 +0800

    [add] 11.盛水最多的容器

---
+++ src/com/garinzhang/algorithm/dynamicprogramming/ContainerWithMostWater.java
@@ -0,0 +1,20 @@
+package com.garinzhang.algorithm.dynamicprogramming;
+
+public class ContainerWithMostWater {
+    public static int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int largest = 0;
+        while (left < right) {
+            largest = height[left] <= height[right]
+                ? Math.max(largest, (right - left) * height[left ++])
+                : Math.max(largest, (right - left) * height[right --]);
+        }
+        return largest;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxArea(new int[] { 1, 1}) == 1);
+    }
+
+}
