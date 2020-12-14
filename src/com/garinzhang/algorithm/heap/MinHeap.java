06345868f2c65cf307f4d5762ca246e2ee40e9af
Garin <454067820@qq.com>
2020-12-14 13:46:34 +0800

    optimize Min Heap Construction

--- src/com/garinzhang/algorithm/heap/MinHeap.java
+++ src/com/garinzhang/algorithm/heap/MinHeap.java
@@ -1,61 +1,49 @@
 package com.garinzhang.algorithm.heap;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * @author Garin
- * @date 2020-10-30
+ * 最大堆
  */
 public class MinHeap {
-    List<Integer> heap = new ArrayList<Integer>();
 
+    List<Integer> heap = new ArrayList<>();
+	
     public MinHeap(List<Integer> array) {
         heap = buildHeap(array);
-        System.out.println(Arrays.toString(heap.toArray()));
     }
 
     public List<Integer> buildHeap(List<Integer> array) {
-        int firstParent = (array.size() - 2) / 2;
-        for (int i = firstParent; i >= 0; i --) {
-            siftDown(firstParent, array.size() - 1, array);
+        int firstParentIdx = (array.size() - 2) / 2;
+        for (int i = firstParentIdx; i >= 0; i --) {
+            siftDown(i, array.size() - 1, array);
         }
         return array;
     }
 
-    public void siftDown(int currentIdx, int endIdx, List<Integer> heap) {
-        int leftIdx = currentIdx * 2 + 1;
-        while (leftIdx <= endIdx) {
-            int rightIdx = currentIdx * 2 + 2 <= endIdx ? currentIdx * 2 + 2 : -1;
-            int toSwapIdx = leftIdx;
-            if (rightIdx != -1 && heap.get(rightIdx) < heap.get(leftIdx)) {
-                toSwapIdx = rightIdx;
+    public void siftDown(int start, int end, List<Integer> heap) {
+        Integer left = start * 2 + 1;
+        while (left <= end) {
+            Integer right = start * 2 + 2 <= end ? start * 2 + 2 : -1;
+            Integer smaller = left;
+            if (right != -1 && heap.get(right) < heap.get(left)) {
+                smaller = right;
             }
-            if (heap.get(toSwapIdx) >= heap.get(currentIdx)) {
+            if (heap.get(start) <= heap.get(smaller)) {
                 return;
             }
-            swap(heap, currentIdx, toSwapIdx);
-            currentIdx = toSwapIdx;
-            leftIdx = currentIdx * 2 + 1;
+            swap(start, smaller, heap);
+            start = smaller;
+            left = smaller * 2 + 1;
         }
     }
 
-    public void siftUp(int currentIdx, List<Integer> heap) {
-        int i = currentIdx;
-        while (i > 0 && heap.get(i) < heap.get((i - 1) / 2)) {
-            swap(heap, i, (i - 1) / 2);
-            i = (i - 1) / 2;
+    public void siftUp(int start, List<Integer> heap) {
+        while (start > 0 && heap.get(start) < heap.get((start - 1) / 2)) {
+            swap(start, (start - 1) / 2, heap);
+            start = (start - 1) / 2;
         }
-    }
-
-    public int peek() {
-        return heap.get(0);
-    }
-
-    public int remove() {
-        swap(heap, 0, heap.size() - 1);
-        int toRemove = heap.remove(heap.size() - 1);
-        siftDown(0, heap.size() - 1, heap);
-        return toRemove;
     }
 
     public void insert(int value) {
@@ -63,14 +51,22 @@ public class MinHeap {
         siftUp(heap.size() - 1, heap);
     }
 
-    public static void swap(List<Integer> heap, Integer i, Integer j) {
-        int tmp = heap.get(i);
-        heap.set(i, heap.get(j));
-        heap.set(j, tmp);
+    public int remove() {
+        swap(0, heap.size() - 1, heap);
+        int toRemove = heap.remove(heap.size() - 1);
+        siftDown(0, heap.size() - 1, heap);
+        return toRemove;
     }
 
-    public static void main(String[] args) {
-        // List<Integer> array = Arrays.asList(48, 12, 24, 7, 8, -5, 24, 391, 24, 56, 2, 6, 8, 41);
-        // MinHeap heap = new MinHeap(array);
+    public int peek() {
+        return heap.get(0);
     }
+
+    public static void swap(int a, int b, List<Integer> array) {
+        Integer tmp = array.get(a);
+        array.set(a, array.get(b));
+        array.set(b, tmp);
+    }
+
+    
 }
