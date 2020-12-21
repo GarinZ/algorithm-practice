cd0907bfba0af29dc5d80715cc9da4b0fdd55d9f
Garin <454067820@qq.com>
2020-12-21 13:18:42 +0800

    [add] kadane's algorithm

---
+++ src/com/garinzhang/algorithm/famous/KadanesAlgorithm.java
@@ -0,0 +1,16 @@
+package com.garinzhang.algorithm.famous;
+
+/**
+ * 剑指 Offer 42. 连续子数组的最大和
+ */
+public class KadanesAlgorithm {
+    public static int findMaxSum(int[] array) {
+        int maxSubarraySum = array[0];
+        int max = array[0];
+        for (int i = 0; i < array.length; i ++) {
+            maxSubarraySum = Math.max(array[i], maxSubarraySum + array[i]);
+            max = Math.max(max, maxSubarraySum);
+        }
+        return max;
+    }
+}
